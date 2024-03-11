8727711cb70a5153efe65893a165a2b9631ab131
Thiago Augusto <taugusto964@gmail.com>
2024-03-11 10:14:26 -0300

    Adição de classe de modelo e serviço
    Adição da classe de  modelo pizza e  um serviço de dados de pizza na memória.

---
+++ PizzaPi/Models/Pizza.cs
@@ -0,0 +1,10 @@
+﻿namespace PizzaPi.Models
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsGlutenFree {  get; set; }
+
+    }
+}
